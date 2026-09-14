ff757a7c84e853d0fe12249736fcdfa79c3db507
andreyplastun <andreyplastun@gmail.com>
2026-09-14 16:34:50 +0500

    Update 4 app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ SHARED_SECRET = os.environ.get("SHARED_SECRET", "change-me")
 
 CAMERA_LINES = {
     "8248BBEPBV1AFE1": {"x1": 0.53, "y1": 0.22, "x2": 1.0, "y2": 0.242},
-    "44245BHPSF5CF18": {"x1": 0.68, "y1": 0.18, "x2": 1.0, "y2": 0.269},
+    "44245BHPSF5CF18": {"x1": 0.75, "y1": 0.396, "x2": 1.0, "y2": 0.381},
 }
 
 LINE_COLOR = (255, 0, 0)
